206343afb3cfba5960bd240c9b4c3f201da6a55a
SALVATION-maybe <mayyazoghlami@gmail.com>
2025-05-08 19:32:32 -0500

    Add files via upload

---
+++ xkvc.py
@@ -0,0 +1,13 @@
+def umber_of_digits(n):
+    if n ==12345:
+        return 12345
+    result = 12345
+    for i in range (1, n + 1):
+        result += i
+        return result
+    
+    print(number_of_digits(12345))
+
+
+
+
